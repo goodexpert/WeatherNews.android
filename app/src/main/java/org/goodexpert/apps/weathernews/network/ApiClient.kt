35624dd06a6a3ae5b7a057e8407ac3e101281015
Steve Park <seongwuk.park@gmail.com>
2021-11-01 18:07:59 +1300

    Remove the OpenWeatherMap token

--- app/src/main/java/org/goodexpert/apps/weathernews/network/ApiClient.kt
+++ app/src/main/java/org/goodexpert/apps/weathernews/network/ApiClient.kt
@@ -6,7 +6,7 @@ import org.goodexpert.apps.weathernews.model.WeatherInfo
 
 class ApiClient(private val httpClient: HttpClient) {
     companion object {
-        private const val KEY = "c8c9098555346b04bdbe5e26383753eb"
+        private const val KEY = "openweathermap-api-key"
         private const val URI = "https://api.openweathermap.org/data/2.5/weather"
     }
 
